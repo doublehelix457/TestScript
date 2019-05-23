9bf183780386b841664a336c6393164160b55131
doublehelix457 <doublehelix457.dev@gmail.com>
2019-05-23 00:29:39 -0600

    Added leaving bolt

--- lightningperk.js
+++ lightningperk.js
@@ -1,9 +1,13 @@
 var manager;
 
 manager.on(events.PlayerJoinEvent, function(event){
-    
     if(event.getPlayer().hasPermission("script.perk.lightning")){
         event.getPlayer().getWorld().strikeLightningEffect(event.getPlayer().getLocation());
     }
-    
-})
+});
+
+manager.on(events.PlayerQuitEvent, function(event){
+     if(event.getPlayer().hasPermission("script.perk.lightning")){
+        event.getPlayer().getWorld().strikeLightningEffect(event.getPlayer().getLocation());
+    }
+});
